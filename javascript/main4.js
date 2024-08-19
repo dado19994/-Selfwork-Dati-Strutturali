aa0f7c21dd2695454ddbb953b609d4d0984ba42a
Davide <dignazio90@gmail.com>
2024-08-19 17:17:07 +0200

    Selfwork Dati Strutturali

---
+++ javascript/main4.js
@@ -0,0 +1,39 @@
+// SELFWORK DATI STRUTTURALI
+
+// Dati gli array
+let array_1 = [
+    ['un', 'per', 'incatenarli.'],
+    ['Anello', 'trovarli,'],
+    ['ghermirli', 'e'],
+    ['gondor', 'mark'],
+  ];
+  
+  let array_2 = [
+    [['trovarli,']],
+    ['tu,', 'sciocchi'],
+    ['tu,', 'sciocchi', ['padron', 'Sauron']],
+    ['nel', ['fuggite', 'gandalf']],
+    [['domarli,', 'passare'], 'buio']
+  ];
+  
+ 
+  let frase = array_1[0][0].charAt(0).toUpperCase() + array_1[0][0].slice(1) + " " + // "Un"
+             array_1[1][0] + " " +  // "Anello"
+             array_1[0][1] + " " +  // "per"
+             array_2[4][0][0] + " " + // "domarli,"
+             array_1[0][0] + " " + // "un"
+             array_1[1][0] + " " +  // "Anello"
+             array_1[0][1] + " " +  // "per"
+             array_1[1][1] + " " +  // "trovarli,"
+             array_1[0][0] + " " + // "un"
+             array_1[1][0] + " " +  // "Anello"
+             array_1[0][1] + " " +  // "per"
+             array_1[2][0] + " " +  // "ghermirli"
+             array_1[2][1] + " " +  // "e"
+             array_2[3][0] + " " +  // "nel"
+             array_2[4][1] + " " +  // "buio"
+             array_1[0][2];         // "incatenarli."
+  
+  // Stampa la frase risultante
+  console.log(frase);
+  
